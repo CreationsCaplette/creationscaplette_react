9663f0ac99798beca7979f3b2bd7f0a59a9a47bf
CreationsCaplette <david.caplette@gmail.com>
2026-08-25 07:48:44 +0200

    Moved resume data into separate files

---
+++ creationscaplette/src/data/languages.js
@@ -0,0 +1,9 @@
+const languages = [
+    'French - native proficiency',
+    'English - bilingual proficiency',
+    'German - limited working proficiency',
+    'Polish - limited working proficiency',
+    'Russian - elementary proficiency',
+]
+
+export default languages;
